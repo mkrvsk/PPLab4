f779cd37ee6214b469432be775f1147b8206c263
Christelle <bilka2004@ukr.net>
2022-11-16 22:48:48 +0200

    lab 7

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+__version__ = '0.1.0'
+from flask import Flask, Response
 from waitress import serve
 
-app=Flask(__name__)
+from moderator import moderator
+from user import user
+from state import state
+# from mod import moderator
+from article import article
+from updatedArticle import updatedArticle
+
+app = Flask(__name__)
+app.register_blueprint(state)
+app.register_blueprint(moderator)
+app.register_blueprint(user)
+app.register_blueprint(article)
+app.register_blueprint(updatedArticle)
+
 
 @app.route("/")
 def index():
@@ -12,7 +26,33 @@ def index():
 def HelloWorld():
     return "<h1>Hello World 12</h1>"
 
+# session = Session()
+# @app.route('/api/v1/article', methods=['POST'])
+# def create_article():
+#     # Get data from request body
+#     data = request.get_json()
 
-if __name__ == '__app__':
-    
-    serve(app)
+#     # Validate input data
+#     try:
+#         ArticleSchema().load(data)
+#     except ValidationError as err:
+#         return jsonify(err.messages), 400
+
+#     # Check if article already exists
+#     exists = session.query(Article.article_id).filter_by(name=data['name']).first()
+#     if exists:
+#         return Response(status=403, response='article with such number already exists.')
+
+#     # Create new article
+#     new_article = Article(name=data['name'], body=data['body'], version=data['version'])
+
+#     # Add new article to db
+#     session.add(new_article)
+#     session.commit()
+
+#     return Response(response='New audience was successfully created!')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # serve(app)
---
+++ dbmodel.py
@@ -0,0 +1,69 @@
+import os
+
+from sqlalchemy import Integer, String, Column, Date, ForeignKey
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, sessionmaker, scoped_session
+
+DB_URL = os.getenv("DB_URL", "mysql+pymysql://user_name:password@host/database_name")
+
+engine = create_engine(DB_URL)
+
+SessionFactory = sessionmaker(bind=engine)
+
+Session = scoped_session(SessionFactory)
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class User(Base):
+    __tablename__ = 'user'
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String(255))
+    firstname = Column(String(255))
+    lastname = Column(String(255))
+    email = Column(String(255))
+    password = Column(String(255))
+
+
+class Moderator(Base):
+    __tablename__ = 'moderator'
+    moderator_id = Column(Integer, primary_key=True)
+    moderatorname=Column(String(255))
+    firstname=Column(String(255))
+    lastname = Column(String(255))
+    email = Column(String(255))
+    password = Column(String(255))
+    moderatorkey = Column(String(255))
+
+
+class Article(Base):
+    __tablename__ = 'article'
+    article_id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    body = Column(String(8000))
+    version = Column(String(255))
+
+
+class State(Base):
+    __tablename__ = 'state'
+    state_id = Column(Integer, primary_key=True)
+    name = Column(String(45))
+
+
+class UpdatedArticle(Base):
+    __tablename__ = 'updated_article'
+    updated_article_id = Column(Integer, primary_key=True)
+    article_id = Column(Integer, ForeignKey('article.article_id'))
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    moderator_id = Column(Integer, ForeignKey('moderator.moderator_id'))
+    state_id = Column(Integer, ForeignKey('state.state_id'))
+    article_body = Column(String(8000))
+    date = Column(Date)
+    status = Column(String(80))
+    article = relationship("Article")
+    user = relationship("User")
+    moderator = relationship("Moderator")
+    state = relationship("State")
