5640dfe35b9101d7b3470a0e7e0e5db6dce2466d
mkrvsk <makarovskiy2004@gmail.com>
2022-12-01 19:59:28 +0200

    .

--- updatedArticle.py
+++ updatedArticle.py
@@ -52,43 +52,6 @@ def create_updatedArticle():
 
     return Response(response='New updatedArticle was successfully created!')
 
-
-# Get article by id
-@updatedArticle.route('/api/v1/updateArticle', methods=['POST'])
-@auth.login_required
-def create_updatedArticle():
-    # Get data from request body
-    data = request.get_json()
-
-    # if auth.username() != session.query(User).filter_by(user_id=data['user_id']).first().username:
-    #     return Response(status=406, response='Access denied')
-
-    # Validate input data
-    try:
-        UpdatedArticleSchema().load(data)
-    except ValidationError as err:
-        return jsonify(err.messages), 400
-
-    # Check if supplied ArticleId correct
-    db_user = session.query(Article).filter_by(article_id=data['article_id']).first()
-    if not db_user:
-        return Response(status=404, response='No article found.')
-    # Create new article
-    new_updatedArticle = UpdatedArticle(updated_article_id = data['updated_article_id'],
-                                        article_id=data['article_id'],
-                                        user_id=data['user_id'],
-                                        moderator_id=data['moderator_id'],
-                                        state_id=data['state_id'],
-                                        article_body=data['article_body'],
-                                        date=data['date']
-                                        )
-
-    # Add new article to db
-    session.add(new_updatedArticle)
-    session.commit()
-
-    return Response(status=200, response='New updatedArticle was successfully created!')
-
 # Get article by id
 @updatedArticle.route('/api/v1/updateArticle/<ArticleId>', methods=['GET'])
 @auth.login_required
